1efda6103905099c771b7aac06abdb8e50e601ad
Hank <ragreenburg@gmail.com>
2020-10-23 09:15:59 -0700

    Added mo stuf

--- tera_scrape.py
+++ tera_scrape.py
@@ -28,3 +28,22 @@ options.add_experimental_option("prefs", prefs)
 # applies the above changes to the webdriver
 driver = webdriver.Chrome(exe_path, options=options)
 
+url = "gdchiller.teraportal.com"
+driver.get(url)
+uid = username
+pwd = password
+user = browser.find_element_by_id("login_mail")
+password = browser.find_element_by_id("login_pass")
+user.clear()
+password.clear()
+user.send_keys(uid)
+password.send_keys(pwd)
+time.sleep(3)
+browser.find_element_by_name("submit").click()#This may or may not work because it seems like the id isn't shown for sign in but it does show the type, name and value and two of those are "submit" and the other is "Login"
+time.sleep(15)
+#After signing in go to this page: https://gdchillers.teraportal.com/tadmin/minister/prepaids/advanced-search
+csv_url = "https://gdchillers.teraportal.com/tadmin/minister/prepaids/advanced-search"
+browser.get(csv_url)
+time.sleep(15)
+browser.find_element_by_id("btn-download").click()
+time.sleep(20)
